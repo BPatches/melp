43688eed2e356fee8cb93a35723f46ec6b1ca9db
Ryan Stauffer <rstauffe@mymail.mines.edu>
2013-11-12 20:09:57 -0700

    added index.php stub

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Melp! Home Page</title>
+</head>
+  <body>  
+	<h1>Melp!</h1>
+	<h2>Home Page</h2>
+	This is the landing page for Melp!. Image submission can be found <a href="uploadForm.php">here</a>. <br/>
+	Maps can be found <a href="maps.php">here</a>.
+    
+  </body>
+</html>
