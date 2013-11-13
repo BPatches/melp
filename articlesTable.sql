51be94c7927d1afee6cd74d12aeace5073071990
Patches Barringer <PBarring@mines.edu>
2013-11-12 19:24:41 -0700

    added test values to the article table

--- articlesTable.sql
+++ articlesTable.sql
@@ -8,3 +8,9 @@ create table articles
 	articleTitle varChar(50) not null
 	# There is clearly going to need to be other stuff here at some point
 );
+
+insert into articles (articleTitle) values
+        ("somewhere awesome"),
+	("somewhere kinda awesome"),
+	("somewhere not awesome")
+;
