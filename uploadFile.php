609e346e98eee96e62937639f527beaa27a7f507
Patches Barringer <pbarring@mines.edu>
2013-11-12 18:46:53 -0700

    saving off file

--- uploadFile.php
+++ uploadFile.php
@@ -8,16 +8,34 @@ echo "    </head>";
 echo "    <body>";
 
 if ($_FILES["file"]["error"] > 0)
-  {
-    echo "    Error: " . $_FILES["file"]["error"] . "<br>";
-  }
+{
+     echo "    Error: " . $_FILES["file"]["error"] . "<br>";
+}
 else
-  {
-    echo "    Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "    Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "    Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "    Stored in: " . $_FILES["file"]["tmp_name"];
-  }
+{
+     echo "    Upload: " . $_FILES["file"]["name"] . "<br>";
+     echo "    Type: " . $_FILES["file"]["type"] . "<br>";
+     echo "    Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+}
+
+$fname = "t8MelpUploads/" . _POST["nameOnPage"];
+$num ="";
+while ( file_exists($fname . $num))
+{
+     if($num == "")
+     {
+	  $num = 1;
+     }
+     else
+     {
+	  $num += 1;
+     }
+}
+$fname = $fname . $num;
+
+move_uploaded_file($_FILES["file"]["tmp_name"],$fname);
+
+echo "    Stored in: " . $fname ;
 
 echo "    </body>";
 echo "</html>";
