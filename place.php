3849a53473225d6ebf79e3695b7c909ee670bdfa
Patches Barringer <PBarring@mines.edu>
2013-12-02 20:37:09 -0700

    fixed syntax of add new establisment

--- place.php
+++ place.php
@@ -4,7 +4,7 @@ $header="<!DOCTYPE html>
 <html>
 <head>
 <meta charset=\"ISO-8859-1\">
-	<link rel=\"stylesheet\" type=\"text/css\" href=\"default.css\" />
+	<link rel=\"stylesheet\" type=\"text/css\" href=\"../default.css\" />
 
 <title>";
 $menBar="</title>
@@ -23,16 +23,16 @@ $menBar="</title>
 	<div id=\"menu\" class=\"container\">
 	<ul>
 	<li>
-		<a title=\"\" accesskey=\"1\" href=\"index.php\">Home Page</a>
+		<a title=\"\" accesskey=\"1\" href=\"../index.php\">Home Page</a>
 	</li>
 	<li>
-		<a title=\"\" accesskey=\"1\" href=\"login.php\">Login</a>
+		<a title=\"\" accesskey=\"1\" href=\"../login.php\">Login</a>
 	</li>
 	<li>
-		<a title=\"\" accesskey=\"1\" href=\"establishment.php\">Establishment</a>
+		<a title=\"\" accesskey=\"1\" href=\"../establishment.php\">Establishment</a>
 	</li>
 	<li>
-		<a title=\"\" accesskey=\"1\" href=\"maps.html\">Map</a>
+		<a title=\"\" accesskey=\"1\" href=\"../maps.html\">Map</a>
 	</li>
 	</div>
 	<div id=\"page\" class=\"container\">";
@@ -42,19 +42,20 @@ $footer="    </div>
 </html>";
 
 $db = new mysqli('localhost','team08','mango','team08');
-$name = $db->$_POST["name"];
+$name = $_POST["name"];
 $contents = $_POST["contents"];
 
 $page = $header."".$name."".$menBar."<h2>".$name."</h2>".$contents;
 
-$my_file = $name.'.html';
+$my_file = "places/".str_replace(" ","_",$name).'.html';
 $handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
 fwrite($handle, $page);
-
-$qu = "INSERT INTO articles (articleName, articleContents) Values (".real_escape_string($name).",",real_escape_string("contents").")";
+$escName = $db->real_escape_string($name);
+$escCont = $db->real_escape_string($contents);
+$qu = "INSERT INTO articles (articleName, articleContents) Values (".$escName.",".$escCont.")";
 $db->query($qu);
 
 $db->close();
 
-header( 'Location: '.$name.'.html' ) ;
+header( 'Location: places/'.str_replace(" ","_",$name).'.html' ) ;
 ?>
